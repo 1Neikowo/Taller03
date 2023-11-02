f5097ddcdcd4b7b6c950a46f8a29421c03c3dda5
sebas <sebasaraki@gmail.com>
2023-11-02 10:21:36 -0300

    nm

--- src/main/java/Pack.java
+++ src/main/java/Pack.java
@@ -5,4 +5,27 @@ public class Pack {
 	Collection<Producto> productos;
 	private int codigo;
 
+	public Pack(int codigo) {
+		this.codigo = codigo;
+		productos = new ArrayList<Producto>();
+	}
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void addProducto(Producto producto) {
+		productos.add(producto);
+	}
+
+	public void removeProducto(Producto producto) {
+		productos.remove(producto);
+	}
+
+	public Collection<Producto> getProductos() {
+		return productos;
+	}
+
+
+
 }
--- src/main/java/Producto.java
+++ src/main/java/Producto.java
@@ -3,4 +3,15 @@ public class Producto {
 	private int codigo;
 	private String tipo;
 
+	public Producto(int codigo, String tipo) {
+		this.codigo = codigo;
+		this.tipo = tipo;
+	}
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+
+
 }
