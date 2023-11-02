dedb7a747a592e40f17c6c1885ab620d29bf3655
1Neikowo <j.silva23@ufromail.cl>
2023-11-02 11:05:54 -0300

    Clases Camion y Flete

--- src/main/java/Camion.java
+++ src/main/java/Camion.java
@@ -2,10 +2,38 @@ import java.util.*;
 
 public class Camion {
 
-	Collection<Flete> fletes;
-	private int codigo;
+	private ArrayList<Flete> fletes;
+	private int codigo=0;
 	private String patente;
 	private String descripcion;
 	private boolean estado;
+	public int getCodigo() {
+		return codigo;
+	}
+	public boolean getEstado() {
+		return estado;
+	}
+	public void setEstado(boolean estado) {
+		this.estado = estado;
+	}
 
+	public Camion(String patente, String descripcion, boolean estado) {
+		this.fletes = new ArrayList<Flete>();
+		this.codigo++;
+		this.patente = patente;
+		this.descripcion = descripcion;
+		this.estado = estado;
+	}
+	public void agregarFlete(Flete flete) {
+		fletes.add(flete);
+	}
+	public void eliminarFlete(Flete flete) {
+		for (int i = 0; i < fletes.size(); i++) {
+			if (flete == fletes.get(i)){
+				fletes.remove(fletes.get(i));
+			}else{
+				System.out.println("Flete no existente");
+			}
+		}
+	}
 }
--- src/main/java/Flete.java
+++ src/main/java/Flete.java
@@ -2,28 +2,28 @@ import java.util.*;
 
 public class Flete {
 
-	Collection<Pack> packs;
-	private int codigo;
+	private ArrayList<Pack> packs;
+	private int codigo =0;
 	private String descripcion;
 
-	/**
-	 * 
-	 * @param producto
-	 * @param codigoPack
-	 */
-	public void agregarProducto(Producto producto, int codigoPack) {
-		// TODO - implement Flete.agregarProducto
-		throw new UnsupportedOperationException();
+	public Flete(Pack pack, String descripcion) {
+		this.packs = new ArrayList<Pack>();
+		packs.add(pack);
+		this.codigo++;
+		this.descripcion = descripcion;
 	}
-
-	/**
-	 * 
-	 * @param codigoProducto
-	 * @param codigoPack
-	 */
-	public void eliminarProductos(int codigoProducto, int codigoPack) {
-		// TODO - implement Flete.eliminarProductos
-		throw new UnsupportedOperationException();
+	public void agregarPack(Pack pack){
+		if (packs.size() <=5){
+			packs.add(pack);
+		}else{
+			System.out.println("El flete está lleno");
+		}
 	}
-
+	public void eliminarPack(Pack pack){
+		if (packs.size() > 1){
+			packs.remove(pack);
+		}else{
+			System.out.println("El flete no puede estar vacío");
+		}
+	}
 }
